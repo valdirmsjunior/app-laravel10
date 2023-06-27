b58e44495f94c6fa4da935a9aaee91ab4f729736
Valdir Júnior <valdirmsjunior@gmail.com>
2023-06-27 17:02:48 -0300

    add padarao Presenter na paginacao

--- app/Repositories/PaginationInterface.php
+++ app/Repositories/PaginationInterface.php
@@ -2,7 +2,16 @@
 
 namespace App\Repositories;
 
-class PaginationInterface 
+interface PaginationInterface 
 {
-    
+    /**
+     * @return stdClass[]
+     */
+    public function items(): array;
+    public function total(): int;
+    public function isFirstPage(): bool;
+    public function isLastPage(): bool;
+    public function currentPage(): int;
+    public function getNumberNextPage(): int;
+    public function getNumberPreviousPage(): int;
 }
